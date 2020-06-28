8af05286454f2f3e94ada7cf970cb574a64ba723
rajathirumal <rajathirumal98@gmail.com>
2020-06-28 15:04:43 +0530

    try catch

--- ABC-HMS/src/com/abc/controllers/AdminDeskController.java
+++ ABC-HMS/src/com/abc/controllers/AdminDeskController.java
@@ -88,38 +88,47 @@ public class AdminDeskController extends HttpServlet {
 		
 		switch (action) {
 		case "createPatient":
-			Patient newPatient = new Patient();
-			String patient_name =  request.getParameter("patient_name");
-			long patient_SSN = Long.parseLong(request.getParameter("patient_SSN"));
-			int  patient_age = Integer.parseInt( request.getParameter("patient_age"));
-			String address = (String) request.getParameter("address");
-			String city = (String) request.getParameter("city");
-			String state = (String) request.getParameter("state");
-			String type_of_room = (String) request.getParameter("type_of_room");
-			newPatient.setPatient_name(patient_name);
-			newPatient.setPatient_SSN(patient_SSN);
-			newPatient.setPatient_age(patient_age);
-			newPatient.setAddress(address);
-			newPatient.setCity(city);
-			newPatient.setState(state);
-			newPatient.setType_of_room(type_of_room);
-			String patient_id =  AdminDeskService.createPatient(newPatient);
-			response.setContentType("text/plain");
-			if(patient_id!=null) {
-				response.getWriter().write(patient_id);
-			}else {
-				response.getWriter().write("failed");
+			try {
+				Patient newPatient = new Patient();
+				String patient_name =  request.getParameter("patient_name");
+				long patient_SSN = Long.parseLong(request.getParameter("patient_SSN"));
+				int  patient_age = Integer.parseInt( request.getParameter("patient_age"));
+				String address = (String) request.getParameter("address");
+				String city = (String) request.getParameter("city");
+				String state = (String) request.getParameter("state");
+				String type_of_room = (String) request.getParameter("type_of_room");
+				newPatient.setPatient_name(patient_name);
+				newPatient.setPatient_SSN(patient_SSN);
+				newPatient.setPatient_age(patient_age);
+				newPatient.setAddress(address);
+				newPatient.setCity(city);
+				newPatient.setState(state);
+				newPatient.setType_of_room(type_of_room);
+				String patient_id =  AdminDeskService.createPatient(newPatient);
+				response.setContentType("text/plain");
+				if(patient_id!=null) {
+					response.getWriter().write(patient_id);
+				}else {
+					response.getWriter().write("failed");
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-			
 			break;
 		case "updatePatient":
-			rd = request.getRequestDispatcher("admindeskJSPs/updatePatient.jsp");
-			rd.forward(request, response);
+			String type = (String) request.getParameter("actionType");
+			if (type.contentEquals("fetch")) {
+				
+			}else {
+				
+			}
 			break;
 		case "deletePatient":
 			rd = request.getRequestDispatcher("admindeskJSPs/deletePatient.jsp");
 			rd.forward(request, response);
 			break;
+		
+		
 		case "viewAllPatients":
 			// pagination
 			int currentPage = Integer.parseInt(request.getParameter("currentPage"));
@@ -130,6 +139,8 @@ public class AdminDeskController extends HttpServlet {
 			rd = request.getRequestDispatcher("admindeskJSPs/viewAllPatients.jsp");
 			rd.forward(request, response);
 			break;
+			
+			
 		case "searchPatient":
 			rd = request.getRequestDispatcher("admindeskJSPs/searchPatient.jsp");
 			rd.forward(request, response);
