421e53cf94b275ecb16be15ca059b61b8fbb9c7b
rajathirumal <rajathirumal98@gmail.com>
2020-06-28 14:47:28 +0530

    post : createPatient object filled

--- ABC-HMS/src/com/abc/controllers/AdminDeskController.java
+++ ABC-HMS/src/com/abc/controllers/AdminDeskController.java
@@ -56,8 +56,6 @@ public class AdminDeskController extends HttpServlet {
 			List<Patient> patients = null;
 			patients = (List<Patient>) AdminDeskService.getPatients(currentPage, recordsPerPage);
 			
-			
-			
 			rd = request.getRequestDispatcher("admindeskJSPs/viewAllPatients.jsp");
 			rd.forward(request, response);
 			break;
@@ -84,7 +82,65 @@ public class AdminDeskController extends HttpServlet {
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd;
+		String action ="";
+		action = request.getParameter("action");
 		
+		switch (action) {
+		case "createPatient":
+			Patient newPatient = new Patient();
+			String patient_name =  request.getParameter("patient_name");
+			long patient_SSN = Long.parseLong(request.getParameter("patient_SSN"));
+			int  patient_age = Integer.parseInt( request.getParameter("patient_age"));
+			String address = (String) request.getParameter("address");
+			String city = (String) request.getParameter("city");
+			String state = (String) request.getParameter("state");
+			String type_of_room = (String) request.getParameter("type_of_room");
+			newPatient.setPatient_name(patient_name);
+			newPatient.setPatient_SSN(patient_SSN);
+			newPatient.setPatient_age(patient_age);
+			newPatient.setAddress(address);
+			newPatient.setCity(city);
+			newPatient.setState(state);
+			newPatient.setType_of_room(type_of_room);
+			break;
+		case "updatePatient":
+			rd = request.getRequestDispatcher("admindeskJSPs/updatePatient.jsp");
+			rd.forward(request, response);
+			break;
+		case "deletePatient":
+			rd = request.getRequestDispatcher("admindeskJSPs/deletePatient.jsp");
+			rd.forward(request, response);
+			break;
+		case "viewAllPatients":
+			// pagination
+			int currentPage = Integer.parseInt(request.getParameter("currentPage"));
+			int recordsPerPage = 5;
+			List<Patient> patients = null;
+			patients = (List<Patient>) AdminDeskService.getPatients(currentPage, recordsPerPage);
+			
+			rd = request.getRequestDispatcher("admindeskJSPs/viewAllPatients.jsp");
+			rd.forward(request, response);
+			break;
+		case "searchPatient":
+			rd = request.getRequestDispatcher("admindeskJSPs/searchPatient.jsp");
+			rd.forward(request, response);
+			break;
+
+		case "findBilling":
+			rd = request.getRequestDispatcher("admindeskJSPs/findBilling.jsp");
+			rd.forward(request, response);
+			break;
+		case "about":
+			rd = request.getRequestDispatcher("about.jsp");
+			rd.forward(request, response);
+			break;
+
+		default:
+			rd = request.getRequestDispatcher("Dashboard.jsp");
+			rd.forward(request, response);
+			break;
+		}
 	}
 
 }
--- ABC-HMS/src/com/abc/services/AdminDeskService.java
+++ ABC-HMS/src/com/abc/services/AdminDeskService.java
@@ -3,12 +3,13 @@ package com.abc.services;
 import java.util.List;
 
 import com.abc.beans.Patient;
+import com.abc.dao.HospitalDAO;
 
 public class AdminDeskService {
 
 	public static List<Patient> getPatients(int currentPage, int recordsPerPage) {
-		// TODO Auto-generated method stub
 		return null;
+	
 	}
 
 }
