b11e71c0d0f8212093179f71d4a1bd5b667d0589
rajathirumal <rajathirumal98@gmail.com>
2020-06-29 11:24:35 +0530

    view active patients.

--- query.sql
+++ query.sql
@@ -20,7 +20,7 @@ PS : DELETE FROM patient WHERE (patient_id = ?); -> patient id
 -- US005 : View all patients
 -- input : patient id
 -- Show only : id, name, age, doj, address = address+city+state
-PS : SELECT * FROM patient LIMIT ?,?; -> start_value,end_value
+PS : SELECT * FROM patient WHERE patient_status='Active' LIMIT ?,?; -> start_value,end_value
 
 -- US006 : Search patient
 -- input : patient id
