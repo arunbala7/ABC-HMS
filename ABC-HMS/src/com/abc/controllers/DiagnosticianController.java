5732e202c77f5f6aad4351b4f38190ba3deb4a8c
rajathirumal <rajathirumal98@gmail.com>
2020-06-30 13:11:47 +0530

    doGet() Diagnostic controller

--- ABC-HMS/src/com/abc/controllers/DiagnosticianController.java
+++ ABC-HMS/src/com/abc/controllers/DiagnosticianController.java
@@ -1,41 +1,57 @@
 package com.abc.controllers;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class DiagnosticianController
- */
+import com.abc.beans.User;
+
 @WebServlet("/DiagnosticianController")
 public class DiagnosticianController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public DiagnosticianController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		RequestDispatcher rd;
+		HttpSession session = request.getSession();
+		User currentUser = (User) session.getAttribute("currentUser");
+		if (currentUser == null) {
+			rd = request.getRequestDispatcher("index.jsp");
+			rd.forward(request, response);
+		}
+		if (currentUser != null && !currentUser.getWorkGroup().contentEquals("diagnostician")) {
+			rd = request.getRequestDispatcher("Dashboard.jsp");
+			rd.forward(request, response);
+		}
+		String action = "";
+		action = request.getParameter("action");
+		switch (action) {
+		case "addDiagnostic":
+			rd = request.getRequestDispatcher("diagnosticianJSPs/addDiagnostic.jsp");
+			rd.forward(request, response);
+			break;
+
+		case "about":
+			rd = request.getRequestDispatcher("about.jsp");
+			rd.forward(request, response);
+			break;
+		default:
+			rd = request.getRequestDispatcher("Dashboard.jsp");
+			rd.forward(request, response);
+			break;
+		}
+
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 	}
 
 }
